b421bdd96586f05efea552fc5b725d226190382d
chuparola2 <142441782+Lucas53635274@users.noreply.github.com>
2025-07-06 23:48:22 -0300

    Update provinha.js

--- provinha.js
+++ provinha.js
@@ -1,8 +1,16 @@
-function times() {
+let timestotais = 0;
+let timesmax = 4;
+let mostraHTML = document.getElementById("mostraHTML")
 
-    let mostraHTML = document.getElementById("mostraHTML")
+function cadstime(){
 
-    let times = prompt("Digite o nome do time que você está: ")
+    if (timestotais >= timesmax) {
+      alert("Limite de 4 times atingido!");
+      return;
+    }
+
+    let nometime = prompt("Digite o nome do seu time: ")
+    let modalidade = prompt("Digite a modalidade que quer fazer: ")
 
     let aluno1 = prompt("Escreva o nome do aluno 1: ")
     let aluno2 = prompt("Escreva o nome do aluno 2: ")
@@ -13,16 +21,21 @@ function times() {
     let idade3 = Number(window.prompt(`Escreva a idade do ${aluno3}`))
 
     let media;
-
-    mostraHTML.innerHTML = `<p> <b>Nome do Time:${times} </b> </p>
-    <p> Primeiro aluno a jogar: ${aluno1}, Idade: ${idade1} </p>
-    <p> Segundo aluno a jogar: ${aluno2}, Idade: ${idade2} </p>
-    <p> Terceiro aluno a jogar: ${aluno3}, Idade: ${idade3}</p>`;
     
     media = (idade1 + idade2 + idade3) / 3;
 
     if (media > 15) {
         alert ("O seu time não poderá participar do interclasse pela média de idade!!")
     }
+    
+    timestotais++;
 
+    let lista = document.getElementById("listatimes");
+    let bloco = document.createElement("div")
+
+    mostraHTML.innerHTML += `<p class="textos">Time: <b>${nometime.toUpperCase()}</b>, Média de Idade: ${media}</p>`;
+    mostraHTML.innerHTML += `<p class="textos">Modalidade: ${modalidade.toUpperCase()}`
+    mostraHTML.innerHTML += `<p class="textos">${aluno1}, ${idade1}</p>`;
+    mostraHTML.innerHTML += `<p class="textos">${aluno2}, ${idade2}</p>`;
+    mostraHTML.innerHTML += `<p class="textos">${aluno3}, ${idade3}</p>`;   
 }
